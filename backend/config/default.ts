9e658d70e3ee612f0a63dbe0f0827a95d768be68
johnpiapian <johnpiapian@gmail.com>
2025-10-29 22:12:54 -0500

    fix: add comment

--- backend/config/default.ts
+++ backend/config/default.ts
@@ -1,4 +1,9 @@
+/**
+ * Create .env.development to override these values during development.
+ * Create .env.production to override these values during production. 
+ */
+
 export default {
-  PORT: process.env.PORT || 3001,
+  PORT: process.env.PORT || 3000,
   MONGODB_URI: process.env.MONGODB_URI || '',
 }
